598c04cbe6512de8e0903404b947f724e0f9a97a
Aniket Raj <aniketraj0224@gmail.com>
2025-05-23 22:24:24 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,35 @@
+class Hello{
+    public static void main(String[] args) {
+        // variables
+        int num1 = 3 ;
+        int num2 = 5 ;
+        int result = num1 + num2;
+        System.out.println(result);
+    
+        // 1
+        System.out.println("Hello, World!");
+
+        // Data Types
+        byte by = 127;
+        System.out.println(by);
+        short sh = 558;
+        System.out.println(sh);
+        long l = 5854l;
+        System.out.println(l);
+        float f = 5.5f;
+        System.out.println(f);
+        double d = 1.7;
+        System.out.println(d);
+        char c = 'S';
+        System.out.println(c);
+        boolean b = true;
+        System.out.println(b);
+        String str = "Spurs are champions.";
+        System.out.println(str);
+
+        // literals
+
+        
+    }
+}
+
